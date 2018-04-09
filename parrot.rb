cc146546c3dd17e5b03fdbc415b78535b747def7
Richard Cho <richardcho100@gmail.com>
2018-04-09 23:14:56 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = squawk)
+def parrot(phrase = "squawk")
   phrase.caplitalize
   puts "#{phrase}!"
-  return "#{phrase}!"
+  #return "#{phrase}!"
 end
