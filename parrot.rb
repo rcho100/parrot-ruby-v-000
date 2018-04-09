6ad2ea95ab8d205d26555e9d29295543e03b6a5a
Richard Cho <richardcho100@gmail.com>
2018-04-09 23:42:54 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+#"Pretty bird!"
+def parrot(phrase = "Squawk!")
+  phrase.capitalize
+  puts "#{phrase}"
+  return "#{phrase}"
+end
